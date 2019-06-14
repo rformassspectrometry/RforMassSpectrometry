811b04826a5f2cffc31bbc3b783cd7bb201397af
Laurent Gatto <laurent.gatto@uclouvain.be>
2019-06-14 10:16:09 +0200

    update startup message

--- R/zzz.R
+++ R/zzz.R
@@ -2,5 +2,6 @@
 .onAttach <- function(libname, pkgname) {
   packageStartupMessage(
       paste("\nThis is RforMassSpectrometry version", packageVersion("RforMassSpectrometry"), "\n",
+            " Please not that it is a very early stage project.\n",
             " Visit https://RforMassSpectrometry.org to get started.\n"))
 }
